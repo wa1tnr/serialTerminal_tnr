6284c035c0d683b22cf3a1cb4ffe1179e9e4f5b4
wa1tnr <wa1tnr@users.noreply.github.com>
2022-07-07 22:40:42 +0000

    interim edit NOT PRODUCTION

--- advanced_terminal/src/js/script.js
+++ advanced_terminal/src/js/script.js
@@ -397,11 +397,15 @@ function printToConsoleln(data, color = "36", array = false) {
 }
 
 function printToConsole(data, color = "36", array = false) {
-  if (data.includes(`\x0a`)) {
+  if (data.includes(
+      `\x0d` ||
+      `\x0a`
+      )
+  ) { // fripp's fresh fish
     terminal.write('~');
-    if (document.getElementById("addLineInBound").checked == true) terminal.write(`\x0a`);  
-    if (document.getElementById("carriageReturnInBound").checked == true) terminal.write(`\x0d`);
-    // terminal.write(`\x0d`);
+    // if (document.getElementById("addLineInBound").checked == true) terminal.write(`\x0a`);  
+    // if (document.getElementById("carriageReturnInBound").checked == true) terminal.write(`\x0d`);
+    terminal.write(`\x0a`);
   } // experiment 7 Jul 22:19z
   terminal.write(`\x1B[0;3;${color}m${data}\x1B[0m`); // << workhorse
 }
